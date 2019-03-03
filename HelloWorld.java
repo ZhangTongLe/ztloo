c25bb3c69978b35b9382e4e88d55cc2524bd853d
zhangtongle <1071235258@qq.com>
2019-03-03 19:14:31 +0800

    this 11

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 
 
     public static void main(String[] args) {
-        System.out.println("hello2");
+        System.out.println("hello1");
         System.out.println("world");
     }
 
